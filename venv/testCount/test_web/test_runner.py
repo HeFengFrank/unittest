3ad0598368cc609029dc89ebdf3500e2a1332415
HeFengFrank <hefeng_19870604>
2018-11-27 17:51:07 +0800

    add a line print message

--- venv/testCount/test_web/test_runner.py
+++ venv/testCount/test_web/test_runner.py
@@ -5,5 +5,6 @@ discover=unittest.defaultTestLoader.discover(test_dir,pattern='test_*.py')
 
 if __name__=='__main__':
     print("hello everbody")
+    print("hello everbody2")
     runner=unittest.TextTestRunner()
     runner.run(discover)
